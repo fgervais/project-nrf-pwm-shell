bb9c11132e6368c214af9208187762824af98cec
Francois Gervais <francoisgervais@gmail.com>
2023-09-07 20:08:43 -0400

    Empty out main()

--- app/src/main.c
+++ app/src/main.c
@@ -10,16 +10,5 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 void main(void)
 {
-	const struct device *cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-
-	LOG_INF("Version: %s", APP_VERSION_FULL);
-
-	LOG_INF("****************************************");
-	LOG_INF("MAIN DONE");
-	LOG_INF("****************************************");
-
-	k_sleep(K_SECONDS(3));
-	// pm_device_action_run(cons, PM_DEVICE_ACTION_SUSPEND);
-
-	LOG_INF("PM_DEVICE_ACTION_SUSPEND");
+	LOG_INF("🚀 MAIN START (%s) 🚀", APP_VERSION_FULL);
 }
